d033ccf7f9f911a138bfce3cb20c52ab9f091939
Galileo Guzman <galileoguzman@gmail.com>
2016-06-20 18:31:18 -0500

    Initial Commit

---
+++ Obesidad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Obesidad
+//
+//  Created by Galileo Guzman on 6/20/16.
+//  Copyright © 2016 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Obesidad/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Obesidad
+//
+//  Created by Galileo Guzman on 6/20/16.
+//  Copyright © 2016 Galileo Guzman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
